6e9fe916373221238fdad6728248039f6e95edee
sakamoto-arifumi <44026430+sakamoto-arifumi@users.noreply.github.com>
2019-07-06 14:34:37 +0900

    Update server.py

--- app/server.py
+++ app/server.py
@@ -9,6 +9,7 @@ from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
 
+
 #export_file_url = 'https://www.dropbox.com/s/6bgq8t6yextloqp/export.pkl?raw=1'
 export_file_url = 'https://drive.google.com/open?id=1ltFJ0iIgRWFz_stPp2XZI9C85pb_ZNPR'
 
