264aa60250005b1bcc6f384f3e210e135118ec03
sakamoto-arifumi <44026430+sakamoto-arifumi@users.noreply.github.com>
2019-07-06 14:17:55 +0900

    Update server.py
    wakumayu識別器に変更

--- app/server.py
+++ app/server.py
@@ -9,10 +9,14 @@ from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
 
-export_file_url = 'https://www.dropbox.com/s/6bgq8t6yextloqp/export.pkl?raw=1'
-export_file_name = 'export.pkl'
+#export_file_url = 'https://www.dropbox.com/s/6bgq8t6yextloqp/export.pkl?raw=1'
+export_file_url = 'https://drive.google.com/open?id=1ltFJ0iIgRWFz_stPp2XZI9C85pb_ZNPR'
 
-classes = ['black', 'grizzly', 'teddys']
+#export_file_name = 'export.pkl'
+export_file_name = 'wakumayu.pkl'
+
+#classes = ['black', 'grizzly', 'teddys']
+classes = ['haru','sasazaki','wakumayu']
 path = Path(__file__).parent
 
 app = Starlette()
